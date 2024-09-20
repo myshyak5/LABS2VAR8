44adfe7d3417174432a3cbd17ca33fc10172738c
StaMeska5 <stameska40@mail.ru>
2024-09-20 21:35:18 +0200

    Add: all files in project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+//Вариант 5 (Объём прямоугольного параллелепипеда)
+int main()
+{
+    int A, B, C;
+    std::cin >> A >> B >> C;
+
+    std::cout << A*B*C << std::endl;
+}
